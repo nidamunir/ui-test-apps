58fbbb2be39247fcaf47783623e8106605d7c643
Nida Munir <nida.munir@emumba.com>
2020-10-22 20:20:21 +0500

    Add new components

---
+++ ui-test-components/src/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const AppButton = ( ) => {
+    return (
+        <button onClick={() => console.log("Button clicked")}>
+            Button
+        </button>
+        
+    );
+};
+
+export default AppButton;
